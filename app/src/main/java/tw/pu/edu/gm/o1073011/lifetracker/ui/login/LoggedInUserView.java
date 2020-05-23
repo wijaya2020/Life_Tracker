cd8795d4e461c062d6655be0b62e5ab443343ec9
o1073011 <65273464+o1073011@users.noreply.github.com>
2020-05-23 16:59:46 +0800

    add new login screen and welcome screen

---
+++ app/src/main/java/tw/pu/edu/gm/o1073011/lifetracker/ui/login/LoggedInUserView.java
@@ -0,0 +1,17 @@
+package tw.pu.edu.gm.o1073011.lifetracker.ui.login;
+
+/**
+ * Class exposing authenticated user details to the UI.
+ */
+class LoggedInUserView {
+    private String displayName;
+    //... other data fields that may be accessible to the UI
+
+    LoggedInUserView(String displayName) {
+        this.displayName = displayName;
+    }
+
+    String getDisplayName() {
+        return displayName;
+    }
+}
