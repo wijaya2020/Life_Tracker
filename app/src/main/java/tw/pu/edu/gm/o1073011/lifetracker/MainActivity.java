11331073ae29d13f6e31897b38b5914f4289abdf
o1073011 <65273464+o1073011@users.noreply.github.com>
2020-05-23 15:07:49 +0800

    Create new file

---
+++ app/src/main/java/tw/pu/edu/gm/o1073011/lifetracker/MainActivity.java
@@ -0,0 +1,14 @@
+package tw.pu.edu.gm.o1073011.lifetracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
